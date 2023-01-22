1a019ff461885f66af418f6d4ae90bc9b7e8779f
lnrdlgtn <leo.ligutanjr@gmail.com>
2023-01-22 21:45:26 +0800

    Data fetching and page rendering using getServeerSideProps()

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,29 @@
-import Head from 'next/head'
-export default function Home() {
-  return ( 
+import Head from "next/head";
+import ArticleList from "@/components/ArticleList";
+
+export default function Home({ articles }) {
+  return (
     <div>
       <Head>
         <title>Blog News</title>
-        <meta name='keywords' content='Blog - Development, Programming' />
+        <meta name="keywords" content="Blog - Development, Programming" />
       </Head>
-      <h1>Welcome to NextJS</h1>
+      
+      <ArticleList articles={articles} />
     </div>
-  )
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
